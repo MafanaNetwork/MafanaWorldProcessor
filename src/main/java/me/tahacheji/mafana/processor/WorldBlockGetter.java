554c87284a4f9f6cf52833d15405756106856626
tahac <wolfprogaming123@gmail.com>
2023-09-17 04:43:24 -0400

    WorldBlock

--- src/main/java/me/tahacheji/mafana/processor/WorldBlockGetter.java
+++ src/main/java/me/tahacheji/mafana/processor/WorldBlockGetter.java
@@ -1,12 +1,11 @@
 package me.tahacheji.mafana.processor;
 
-import me.tahacheji.mafana.MafanaWorldProcessor;
+import me.tahacheji.mafana.processor.Cube;
+import me.tahacheji.mafana.processor.CubeDivider;
+import me.tahacheji.mafana.processor.WorldBlock;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -26,60 +25,26 @@ public class WorldBlockGetter {
         CompletableFuture<List<WorldBlock>> future = new CompletableFuture<>();
         CubeDivider divider = new CubeDivider(x, y);
         List<Cube> smallerCubes = divider.divide(divisions);
-        List<CompletableFuture<List<WorldBlock>>> cubeFutures = new ArrayList<>();
+
+        List<WorldBlock> allBlocks = new ArrayList<>();
 
         for (Cube cube : smallerCubes) {
-            List<Location> cubeLocations = cube.getLocations();
-            CompletableFuture<List<WorldBlock>> cubeFuture = processCubeAsync(cubeLocations);
-            cubeFutures.add(cubeFuture);
+            List<WorldBlock> cubeBlocks = getWorldBlocksInCube(cube);
+            allBlocks.addAll(cubeBlocks);
         }
 
-        CompletableFuture<Void> allOf = CompletableFuture.allOf(cubeFutures.toArray(new CompletableFuture[0]));
-
-        allOf.thenAccept(result -> {
-            List<WorldBlock> allBlocks = new ArrayList<>();
-            for (CompletableFuture<List<WorldBlock>> cubeFuture : cubeFutures) {
-                try {
-                    allBlocks.addAll(cubeFuture.get());
-                } catch (Exception e) {
-                    // Handle exceptions if needed
-                }
-            }
-            future.complete(allBlocks);
-        });
-
+        future.complete(allBlocks);
         return future;
     }
 
-    private CompletableFuture<List<WorldBlock>> processCubeAsync(List<Location> locations) {
-        CompletableFuture<List<WorldBlock>> future = new CompletableFuture<>();
-
-        for (Location location : locations) {
-            BukkitTask task = new BukkitRunnable() {
-                @Override
-                public void run() {
-                    List<WorldBlock> worldBlocks = getWorldBlocksInCube(location, 1, 1, 1);
-                    future.complete(worldBlocks);
-                }
-            }.runTask(MafanaWorldProcessor.getInstance());
-        }
-
-        return future;
-    }
-
-    private List<WorldBlock> getWorldBlocksInCube(Location corner, int width, int height, int depth) {
+    private List<WorldBlock> getWorldBlocksInCube(Cube cube) {
         List<WorldBlock> worldBlocks = new ArrayList<>();
-        World world = corner.getWorld();
+        World world = x.getWorld();
 
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                for (int z = 0; z < depth; z++) {
-                    Location location = new Location(world, corner.getX() + x, corner.getY() + y, corner.getZ() + z);
-                    // Assuming you have a method to retrieve Material at a location
-                    Material material = getMaterialAtLocation(location);
-                    worldBlocks.add(new WorldBlock(material, location.getBlockX(), location.getBlockY(), location.getBlockZ()));
-                }
-            }
+        // Iterate through locations within the cube and retrieve materials
+        for (Location location : cube.getLocations()) {
+            Material material = getMaterialAtLocation(location);
+            worldBlocks.add(new WorldBlock(material, location.getBlockX(), location.getBlockY(), location.getBlockZ()));
         }
 
         return worldBlocks;

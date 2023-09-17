8bb37c7d6926c9d86512436e8207cb4d6d41a13f
tahac <wolfprogaming123@gmail.com>
2023-09-17 06:01:37 -0400

    WorldBlock

--- src/main/java/me/tahacheji/mafana/processor/WorldBlockUtil.java
+++ src/main/java/me/tahacheji/mafana/processor/WorldBlockUtil.java
@@ -1,5 +1,7 @@
 package me.tahacheji.mafana.processor;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.bukkit.Material;
 
 import java.io.ByteArrayInputStream;
@@ -13,48 +15,13 @@ import java.util.zip.GZIPOutputStream;
 
 public class WorldBlockUtil {
 
-    public String compressDungeonWorldBlocks(List<WorldBlock> blocks) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        try (GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream)) {
-            for (WorldBlock worldBlock : blocks) {
-                String material = worldBlock.getMaterial().name();
-                String x = String.valueOf(worldBlock.getX());
-                String y = String.valueOf(worldBlock.getY());
-                String z = String.valueOf(worldBlock.getZ());
-                String entry = material + "|" + x + "|" + y + "|" + z + "\n";
-                gzipOutputStream.write(entry.getBytes());
-            }
-        }
-        return byteArrayOutputStream.toString("UTF-8");
+    public List<WorldBlock> decompressJsonToWorldBlocks(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, new TypeToken<List<WorldBlock>>() {}.getType());
     }
 
-    public List<WorldBlock> decompressDungeonWorldBlocks(String compressedData) throws IOException {
-        List<WorldBlock> worldBlocks = new ArrayList<>();
-        byte[] compressedBytes = compressedData.getBytes(StandardCharsets.UTF_8);
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(compressedBytes);
-        try (GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream)) {
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            StringBuilder entry = new StringBuilder();
-
-            while ((bytesRead = gzipInputStream.read(buffer)) != -1) {
-                entry.append(new String(buffer, 0, bytesRead, StandardCharsets.UTF_8));
-                while (entry.toString().contains("\n")) {
-                    int endIndex = entry.indexOf("\n");
-                    String line = entry.substring(0, endIndex);
-                    entry.delete(0, endIndex + 1);
-
-                    String[] parts = line.split("\\|");
-                    if (parts.length >= 4) {
-                        Material material = Material.valueOf(parts[0]);
-                        int x = Integer.parseInt(parts[1]);
-                        int y = Integer.parseInt(parts[2]);
-                        int z = Integer.parseInt(parts[3]);
-                        worldBlocks.add(new WorldBlock(material, x, y, z));
-                    }
-                }
-            }
-        }
-        return worldBlocks;
+    public String compressWorldBlocksToJson(List<WorldBlock> worldBlocks) {
+        Gson gson = new Gson();
+        return gson.toJson(worldBlocks);
     }
 }
